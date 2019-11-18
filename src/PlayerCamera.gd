f30fc40cb37ee453be454a7b0c185e96c455d9ed
James Fraser-Jones <jf14218@my.bristol.ac.uk>
2019-11-18 18:22:41 +0000

    smooth camera bby

--- src/PlayerCamera.gd
+++ src/PlayerCamera.gd
@@ -1,20 +1,39 @@
 extends Camera
 
+export var zoom_min = 0
+export var zoom_max = 2
+export var zoom_inc = 0.2
+export var zoom_speed = 5
+
 var initSize = size #20
 var initFOV = fov 	#50
-
 var zoom = 1
-var zoom_min = 0
-var zoom_max = 2
-var zoom_inc = 0.2
+
+enum {IN, OUT}
+
+var zoom_dir = IN
+var zoom_degree = 0
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("num_centre"):
 		toggleProjection()
 	if event.is_action_pressed("num_top_right"):
-		zoom(-zoom_inc)
+		zoom_dir = IN
+		zoom_degree = zoom_inc
 	if event.is_action_pressed("num_bottom_right"):
-		zoom(zoom_inc)
+		zoom_dir = OUT
+		zoom_degree = zoom_inc
+		
+func _process(delta: float) -> void:
+	if zoom_degree > 0:
+		var zoom_chunk = min(zoom_inc * zoom_speed * delta, zoom_degree)
+		zoom(zoom_chunk if zoom_dir == OUT else -zoom_chunk)
+		zoom_degree -= zoom_chunk
+
+func zoom(inc: float) -> void:
+	zoom = clamp(zoom + inc, zoom_min, zoom_max)
+	size = initSize * zoom
+	fov = initFOV * zoom
 
 func toggleProjection() -> void:
 	match projection:
@@ -22,8 +41,3 @@ func toggleProjection() -> void:
 			set_orthogonal(size, near, far)
 		PROJECTION_ORTHOGONAL:
 			set_perspective(fov, near, far)
-
-func zoom(inc: float) -> void:
-	zoom = clamp(zoom + inc, zoom_min, zoom_max)
-	size = initSize * zoom
-	fov = initFOV * zoom
--- src/PlayerCameraPivot.gd
+++ src/PlayerCameraPivot.gd
@@ -1,17 +1,52 @@
 extends Spatial
 
-var increment = 15
-var x_lock = 45
+export var x_lock = 45
+export var x_inc = 15
+export var x_speed = 5
+export var y_inc = 90
+export var y_speed = 5
+
+enum {UP, DOWN, LEFT, RIGHT}
+
+var turn_dir = UP
+var turn_degree = 0
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("num_left"):
-		rotate(Vector3.UP, toRad(-increment))
-	if event.is_action_pressed("num_right"):
-		rotate(Vector3.UP, toRad(increment))
-	if event.is_action_pressed("num_up"):
-		rotate_object_local(Vector3.RIGHT, toRad(max(-increment, -x_lock - rotation_degrees.x)))
-	if event.is_action_pressed("num_down"):
-		rotate_object_local(Vector3.RIGHT, toRad(min(increment, x_lock - rotation_degrees.x)))
-		
+	if turn_degree <= 0:
+		if event.is_action_pressed("num_left"):
+			turn_dir = LEFT
+			turn_degree = y_inc
+		if event.is_action_pressed("num_right"):
+			turn_dir = RIGHT
+			turn_degree = y_inc
+		if event.is_action_pressed("num_up"):
+			turn_dir = UP
+			turn_degree = x_inc
+		if event.is_action_pressed("num_down"):
+			turn_dir = DOWN
+			turn_degree = x_inc
+
+func _process(delta: float) -> void:
+	if turn_degree > 0:
+		var turn_inc
+		var turn_speed
+		match turn_dir:
+			UP, DOWN:
+				turn_inc = x_inc
+				turn_speed = x_speed
+			LEFT, RIGHT:
+				turn_inc = y_inc
+				turn_speed = y_speed
+		var turn_chunk = min(turn_inc * turn_speed * delta, turn_degree)
+		turn_camera(turn_dir, turn_chunk, x_lock)
+		turn_degree -= turn_chunk
+
+func turn_camera(dir: int, deg: float, x_lock: float) -> void:
+	match dir:
+		UP: rotate_object_local(Vector3.RIGHT, toRad(max(-deg, -x_lock - rotation_degrees.x)))
+		DOWN: rotate_object_local(Vector3.RIGHT, toRad(min(deg, x_lock - rotation_degrees.x)))
+		LEFT: rotate(Vector3.UP, toRad(-deg))
+		RIGHT: rotate(Vector3.UP, toRad(deg))
+
 func toRad(deg: float) -> float:
 	return deg * PI / 180
